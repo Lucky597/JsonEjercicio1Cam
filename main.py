38c26f6c8bfc2c7c84792ba64c255d7b67fd708c
versep17 <versep17@gmail.com>
2025-03-05 18:18:03 -0500

    Leer y convertir JSON a estructuras de datos de Python {} []

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+# El json que vamos leer parece ser una lista de Diccionarios
 '''def leer_json(ruta):
     with open(ruta, "r", encoding="utf-8") as archivo:
         return json.load(archivo)
@@ -7,11 +7,17 @@ import json
 datos = leer_json("indicadores.json")
 print(datos)'''
 
-def leer_json(ruta):
-    archivo = open(ruta, "r", encoding="utf-8")
-    datos = json.load(archivo)
+def leer_json(ruta): #Definimos la funcion y colocamos la ruta hasta la extencion del archivo
+    archivo = open(ruta, "r", encoding="utf-8") #Usamos la funcion open para abrir el archivo y r para perisos de lectura en codificacion utf-8, almacenamos en variable archivo
+    datos = json.load(archivo) # almacenamos en variable datos, leemos y convertir un archivo JSON en una estructura de datos de Python {} [].
     archivo.close()  # Cierra el archivo manualmente
-    return datos
+    return datos #Si el JSON contiene {} → se convierte en un diccionario (dict). Si el JSON contiene [] → se convierte en una lista (list).
 
 datos = leer_json("indicadores.json")
+print(datos)
+
+datos = leer_json("paises.json")
+print(datos)
+
+datos = leer_json("poblacion.json")
 print(datos)
